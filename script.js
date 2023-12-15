88cffdd9cba7ef9c4b4abe2d99f8f6224624ab95
unknown <salmanurrehman360@gmail.com>
2023-12-15 21:34:12 +0500

    Added Funcionalities

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let myGif = document.getElementById('gif');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 
+
 let songs = [
     { songName: "Song1", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
     { songName: "Song2", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
@@ -42,14 +43,6 @@ masterPlay.addEventListener('click', ()=>{
 
 })
 
-next.addEventListener('click', ()=>{
-    songIndex+= 1;
-    console.log(songIndex)
-})
-backward.addEventListener('click', ()=>{
-    songIndex-= 1;
-    console.log(songIndex)
-})
 
 
 //Seek progress bar
@@ -73,11 +66,35 @@ const makeAllPlays = ()=>{
 Array.from(document.getElementsByClassName('song-item-play')).forEach((element)=>{
     element.addEventListener('click', (e)=>{
         makeAllPlays();
+        songIndex =parseInt(e.target.id)
         e.target.classList.remove('fa-circle-play')
         e.target.classList.add('fa-circle-pause')
-        audioElement.src='songs/3.mp3'
+        Songnametag.innerText=songs[songIndex].songName;
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add("fa-circle-pause")
+        audioElement.src=  `songs/${songIndex+1}.mp3`
         audioElement.currentTime=0
         audioElement.play()
+
     
     })
+   
+    
+})
+next.addEventListener('click', ()=>{
+    if(songIndex<5){
+    songIndex+= 1;
+    Songnametag.innerText=songs[songIndex].songName;
+    audioElement.src=  `songs/${songIndex+1}.mp3`
+    audioElement.currentTime=0
+    audioElement.play()}
+})
+backward.addEventListener('click', ()=>{
+    if(songIndex>0){
+    songIndex-= 1;
+    console.log(songIndex)
+    Songnametag.innerText=songs[songIndex].songName;
+    audioElement.src=  `songs/${songIndex+1}.mp3`
+    audioElement.currentTime=0
+    audioElement.play()}
 })
